1d0c243f617525495c7bb7ab74a2fd99415bc116
Holger Christiansen <vragour@users.noreply.github.com>
2019-05-23 09:44:33 +0200

    Add files via upload

---
+++ dbooking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBmanager dBManager = new DBmanager();
+
+            List<AfdRessObj> listRessAfdObj = new List<AfdRessObj>();
+            listRessAfdObj =  dBManager.FindLedigeResourcerForAfdeling();
+            foreach (AfdRessObj obj in listRessAfdObj)
+            {
+                Console.WriteLine(obj.Name + "\t" + obj.Pris + "\t" + obj.Adresse + "\t" + obj.PostNr);
+            }
+        }
+    }
+}
