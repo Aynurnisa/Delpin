5456c8126289c7c7632a2462b7eb55a517ca9d30
Balthazar-WGZ7@hotmail.com <Balthazar-WGZ7@hotmail.com>
2019-05-29 08:50:19 +0200

    Change to DBManger (Mich)

--- Delpin/DBManager.cs
+++ Delpin/DBManager.cs
@@ -118,11 +118,11 @@ namespace Delpin
             
             SqlCommand com = new SqlCommand();
             com.Connection = conn;
-            
+
             string sql = "select r.Navn, r.rnr, r.Maerke, r.Pris, a.Adresse, a.Postnr " +
-                         "from Ressourcer r join Afdeling a on not exists" +
-                         "(select '' from reserveret rs where rs.rnr = r.rnr)";
-            
+                         "from v2_Ressourcer r join v2_Afdeling a on not exists" +
+                         "(select '' from v2_Reservation_Line_Ressourcer rs where rs.rnr = r.rnr)";
+
             com.CommandText = sql;
             
             List<AfdRessObj> ressources = new List<AfdRessObj>();
